72665f0b589b223baf00e5866a381ff38002dbe6
abrahamalanya <alanyaoficial@gmail.com>
2024-05-21 15:50:27 -0500

    feat: add rating

--- resources/views/components/movie-card.blade.php
+++ resources/views/components/movie-card.blade.php
@@ -1,9 +1,19 @@
-<div class="w-[125px] cursor-pointer md:w-[calc(25%-15px)] lg:w-[calc(20%-16px)] shrink-0">
+<section class="w-[125px] cursor-pointer md:w-[calc(25%-15px)] lg:w-[calc(20%-16px)] shrink-0">
     <div class="relative w-full aspect-[1/1.5] bg-cover bg-center mb-[30px] flex items-end justify-between p-[10px]">
         <img src="https://image.tmdb.org/t/p/original/4vc8wOf2yG9TiXoJpvz2fJHOmHA.jpg" alt="" class="absolute top-0 left-0 w-full h-full rounded-xl overflow-hidden">
+        <div class="relative top-[30px] bg-white text-slate-900 rounded-full border-2">
+            <div class="relative w-10 h-10 md:w-[50px] md:h-[50px] rounded-full border-4 border-yellow-600 p-[4px] flex justify-center content-center">
+                <span class="text-[1rem] md:text-[1.3rem] font-bold">7.3</span>
+            </div>
+        </div>
+        <div class="w-full relative flex gap-[5px] flex-wrap justify-end">
+            <span class="relative flex flex-wrap justify-end bg-red-500 py-[3px] px-[5px] text-xs rounded text-white whitespace-nowrap">Science Fiction</span>
+            <span class="relative flex flex-wrap justify-end bg-red-500 py-[3px] px-[5px] text-xs rounded text-white whitespace-nowrap">Action</span>
+            <span class="relative flex flex-wrap justify-end bg-red-500 py-[3px] px-[5px] text-xs rounded text-white whitespace-nowrap">Adventure</span>
+        </div>
     </div>
     <div class="flex flex-col">
-        <span class="text-xs md:text-xl mb-[10px] leading-6 truncate">Challengers</span>
-        <span class="text-[14px] opacity-50">Apr 18, 2024</span>
+        <span class="text-xs md:text-xl mb-[10px] leading-6 truncate font-semibold">Challengers</span>
+        <span class="text-[14px] opacity-50 font-semibold">Apr 18, 2024</span>
     </div>
-</div>
+</section>
