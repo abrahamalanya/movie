73fb888a6d8943c974bda116c6f583bfa4335923
abrahamalanya <alanyaoficial@gmail.com>
2024-05-20 23:31:43 -0500

    feat: add component

---
+++ resources/views/components/web-container.blade.php
@@ -0,0 +1,3 @@
+<section {{ $attributes->merge(['class' => 'w-full max-w-[1200px] mx-auto px-5']) }}>
+    {{ $slot }}
+</section>
---
+++ resources/views/components/web-header.blade.php
@@ -0,0 +1,13 @@
+<header class="w-full h-[60px] fixed z-[10] flex items-center bg-slate-900/[0.9]">
+    <x-web-container class="flex items-center justify-between">
+        <div>
+            <a href="/" class="h-[50px]">
+                <img src="{{ asset('assets/movix-logo.svg') }}" alt="">
+            </a>
+        </div>
+        <div class="flex">
+            <a href="/" class="mx-[15px] text-white font-bold hover:text-red-400">Movie</a>
+            <a href="#" class="mx-[15px] text-white font-bold hover:text-red-400">Tv Shows</a>
+        </div>
+    </x-web-container>
+</header>
